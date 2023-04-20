d7edb7cb25a8f9ef43adfc7455db1bbfd59648f6
InmaGalvez99 <ingal99@hotmail.com>
2023-04-20 12:01:37 +0200

    package

---
+++ Intermediate/07_python_package_manager.py
@@ -0,0 +1,30 @@
+#### LIBRERIAS
+
+# PIP
+
+import numpy  # pip intall numpy
+
+print(numpy.version.version)
+
+numpy_array = numpy.array([35, 24, 62, 52, 30, 30, 17])
+print(type(numpy_array))
+print(numpy_array * 2 )
+
+import pandas # pip intall pandas
+
+
+# pip list
+# pip unisntall pandas
+# pip show numpy
+
+# pip intall requests 
+
+import requests # hacer una petición web
+#requests.get()
+
+# arithmetics package
+
+from mypackage import arithmetics 
+
+print(arithmetics.sum_two_values(1, 4))
+
---
+++ Intermediate/mypackage/arithmetics.py
@@ -0,0 +1,4 @@
+### ARITHMETICS
+
+def sum_two_values (first_value, second_values):
+    return first_value + second_values
