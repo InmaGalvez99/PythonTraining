3bbf4f70a4e5e68cff745a3f8ac156458c6a7e71
InmaGalvez99 <ingal99@hotmail.com>
2023-05-15 12:52:51 +0200

    ejercicio 3

--- Exercises/Más_ejercicios.py
+++ Exercises/Más_ejercicios.py
@@ -1,5 +1,5 @@
 """
-Escribir un programa que lea un numero par por teclado. Si el usuario introduce un numero impar, debe repetirse el proceso hasta que lo introduzca correctamente."""
+1. Escribir un programa que lea un numero par por teclado. Si el usuario introduce un numero impar, debe repetirse el proceso hasta que lo introduzca correctamente."""
 
 while True:
     numero= int(input("introduce un numero par: "))
@@ -12,7 +12,7 @@ while True:
 
 
 """
-Escribir un programa que pida el usuario cuantos numeros quiere introducir. Luego, que lea todos los numeros y realice una media aritmetica """
+2. Escribir un programa que pida el usuario cuantos numeros quiere introducir. Luego, que lea todos los numeros y realice una media aritmetica """
 
 num = int(input("introduce una cantidad de números: "))
 numeros = []
@@ -21,3 +21,15 @@ for i in range(num):
     numeros.append(m)
 
 print("La media de los numeros es ", sum(numeros)/len(numeros))
+
+
+"""
+3. Utilizando la función range() y la conversión a listas, generar las siguientes listas dinámicamente:
+Todos los números pares del 0 al 20
+Todos los números múltiplos de 5 del 0 al 50
+"""
+
+print([x for x in range(0, 21) if x%2 == 0]) ## lista de pares
+print([x for x in range(0,51) if x%5 == 0]) ## lista de múltiplos
+
+
